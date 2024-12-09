0fb577986d3e350f84c9a4d2e6a3f31e4ecc3018
jaggdl <ja125garcia@gmail.com>
2024-12-09 01:08:03 -0600

    Trasparent theme background

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -116,9 +116,11 @@ require("lazy").setup({
     },
     {
       "tiagovla/tokyodark.nvim",
-      opts = {
-        -- custom options here
-      },
+      opts = function()
+        return {
+          transparent_background = true,
+        }
+      end,
       config = function(_, opts)
         require("tokyodark").setup(opts) -- calling setup is optional
         vim.cmd([[colorscheme tokyodark]])
