dee6805905f81135c615fe9c6295d410808dba3d
jaggdl <ja125garcia@gmail.com>
2025-02-05 23:56:51 -0600

    Delete avante

--- init.lua
+++ init.lua
@@ -1,14 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-require("telescope").setup({
-  defaults = {
-    file_ignore_patterns = {
-      "node_modules",
-    },
-  },
-})
-
 vim.g.neoformat_try_node_exe = 1
 
 vim.api.nvim_create_autocmd("BufWritePre", {
