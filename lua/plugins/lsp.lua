202961cf4dcaf192497e18fa4fbb06e3e2becdcb
jaggdl <ja125garcia@gmail.com>
2025-03-24 12:30:31 -0600

    Better lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,11 @@
 return {
   {
+
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
     opts = {
       servers = {
         -- For Typescript/Vue setup
@@ -26,42 +27,7 @@ return {
         cssls = {},
         eslint = {},
         tflint = {},
-        ruby_lsp = {
-          mason = false,
-          cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
-        },
-        rubocop = {
-          cmd = { "bundle", "exec", "rubocop", "--lsp" },
-          filetypes = { "ruby" },
-          root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
-        },
       },
     },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local null_ls = require("null-ls")
-
-      opts.sources = opts.sources or {}
-      vim.list_extend(opts.sources, {
-        -- Prettier (with project local node_modules)
-        null_ls.builtins.formatting.prettier.with({
-          command = "node_modules/.bin/prettier",
-          filetypes = { "html", "markdown", "css", "typescript", "vue" },
-        }),
-        -- stylua for Lua
-        null_ls.builtins.formatting.stylua,
-        -- clang_format for C/C++
-        null_ls.builtins.formatting.clang_format,
-        -- rubocop for Ruby diagnostics (linting) and formatting
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
-
-        null_ls.builtins.formatting.terraform_fmt,
-        null_ls.builtins.formatting.terrafmt, -- markdown
-        null_ls.builtins.diagnostics.terraform_validate,
-      })
-    end,
   },
 }
